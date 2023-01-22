4f30520c127352965f4aa28ed7778489369d62cb
codeDiM0N <karpenko.19dima95@yandex.ru>
2023-01-23 02:07:25 +0300

    22/07/2023

--- HOMEWORK/homework 5/Program.cs
+++ HOMEWORK/homework 5/Program.cs
@@ -40,8 +40,8 @@ void ShowArray(int[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
-
 */
+/*
 //--------------------------------------------------------------------------------------------------
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
@@ -51,57 +51,36 @@ void ShowArray(int[] numbers)
 //--------------------------------------------------------------------------------------------------
 
 
-/*
-Console.WriteLine("Введите длину массива:  ");
-int size = Convert.ToInt32(Console.ReadLine());
-//int[] num = new int[size]; // объявил размер массива
-
-CreateRandonNumbers(size);
-Console.WriteLine("В этом массиве: ");
-//ShowArray(num);
-
-void CreateRandonNumbers (int num)
+int[] CreateArrayRandom(int size)
 {
-    int[] array = new int[size];// объявил размер массива
-    for(int i = 0; i < size; i++)
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(-100,100);
+        array[i] = new Random().Next(1, 100);
     }
-
-
-
-int count = 0; // сумма нечетных чисел
-
-for (int x = 0; x <= array.Length; x++)
-{
-if (array[x] % 2 == 0) //<----------ОШИБКА!!!
-// как я вижу и перевод 
-// если в массивe "array" число под индексом[x] разделить с остатком на 2 и частное будет равно 0 ,то тогда выполнить условие
-// индекс находится за пределами массива!!!! WTF индекс начинается с 0 и заканчивается размером массива!!!!
-// WTF индекс начинается с 0 и заканчивается размером массива!!!!
-count = count + array[x];
+    return array;
 }
-Console.WriteLine($"сумма элементов стоящих на нечетных позициях равна + {count}");
-}
-/*
-void ShowArray(int[] num)
+
+void ShowArray(int[] arr)
 {
-    Console.Write("[ ");
-    for(int i = 0; i < num.Length; i++)
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write(num[i] + " ");
+        Console.Write(arr[i] + " ");
+        if (i % 2  != 0) 
+        {
+        sum += arr[i];
+        }
     }
-    Console.Write("]");
+    Console.WriteLine();
+    Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
     Console.WriteLine();
 }
+Console.WriteLine("Введите длину массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+ShowArray(CreateArrayRandom(size));
 */
-
-
-
-
-
-
-
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 //[3 7 22 2 78] -> 76
@@ -119,34 +98,36 @@ int[] CraeteArray(int size) //создание массива
     return array;
 }
 
-void showArray(int[] array) //метод возврата массива
+void ShowArray(int[] array) //метод возврата массива
+
 {   
+    int result = 0;
     Console.Write("[ ");
     for(int i=0;i<array.Length;i++)
     {
         Console.Write(+array[i] + " ");
     }
     Console.Write("]");
-    Console.WriteLine();        
+    Console.WriteLine();   
+
+    result = array[array.Length]-array[0]; //<-------- ОШИБКА!!!! Index was outside the bounds of the array.   
 }
-int Method(int[] array) //главный метод
+/*
+int[] Method(int[] array) //главный метод
 {
     int Min= array[0];
     int Max= array[array.Length];// <----- ОШИБКА почему индекс за гранью массива когда он должен быть равен длине массива
-    int result = Min-Max;
+    int result = array[array.Length]-array[0];
     return result;
 }
-
+*/
 Console.WriteLine("Введите длину массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int result = 0;
 int[] newArray = CraeteArray(size);
-showArray(newArray);
-Method(newArray);
+ShowArray(newArray);
+//Method(newArray);
 
 
-
-//135 77 //Index was outside the bounds of the array.
-
-
+ //Index was outside the bounds of the array.
 
